4522adb7fce977bbb80cb90e3300cc3f9ec5c595
Chuks <63956312+skido600@users.noreply.github.com>
2024-11-25 00:28:37 +0100

    post destination wrking

--- src/Components/pages/Shorten.tsx
+++ src/Components/pages/Shorten.tsx
@@ -5,13 +5,12 @@ const Shorten: React.FC = () => {
   const [value, setValue] = useState<string>("");
   const [error, setError] = useState<string | null>(null);
   const [loader, setLoader] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [shortenedLink, setShortenedLink] = useState<string | null>(null);
+  const [copied, setCopied] = useState<boolean>(false);
 
   const urlRegex =
     /^(https?:\/\/)?([\w\-]+\.)+[a-z]{2,6}(\/[\w\-._~:\/?#[\]@!$&'()*+,;=]*)?$/i;
-
-  const body = {
-    destination: value,
-  };
 
   const handleShorten = async () => {
     if (!urlRegex.test(value)) {
@@ -23,48 +22,56 @@ const Shorten: React.FC = () => {
     setLoader(true);
 
     try {
-      const response = await fetch("https://short-xish.onrender.com/api/url", {
+      const response = await fetch("https://api.tinyurl.com/create", {
         method: "POST",
         headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify({ url: value }),
       });
 
-      if (response.status === 201) {
-        const data = await response.json();
-        console.log("Shortened URL:", data);
-      } else if (response.status === 400) {
-        setError("Destination URL is required");
-      } else if (response.status === 500) {
-        setError("Internal server error");
-      } else {
-        setError("An unknown error occurred. Please try again.");
+      if (!response.ok) {
+        throw new Error(`Failed with status ${response.status}`);
       }
-    } catch (error) {
-      setError("Something went wrong. Please try again later.");
+
+      const data = await response.json();
+
+      setSuccess(true);
+      setCopied(false);
+      setShortenedLink(data.data.tiny_url);
+      setValue("");
+    } catch (error: any) {
+      setError("Something went wrong. " + error.message);
       console.error("Error:", error);
     } finally {
       setLoader(false);
     }
   };
 
+  const handleCopy = () => {
+    if (shortenedLink) {
+      navigator.clipboard.writeText(shortenedLink);
+      setCopied(true);
+    }
+  };
+
   return (
-    <main>
-      <section className="bg-custom-bg md:mx-8 mx-2 relative mb-4 h-[178px] mt-4 lg:mx-[9.3rem] overflow-hidden rounded-lg">
+    <main className="md:mx-8 mx-2 lg:mx-[9.3rem]">
+      <section className="bg-custom-bg relative mb-4 h-[190px] mt-4 overflow-hidden rounded-lg">
         <div className="md:grid md:grid-cols-3 gap-4 mx-4 items-center mt-[2rem] md:mt-[5rem] justify-center pl-4">
           <div className="md:col-span-2 mb-6 md:mb-0">
             <input
               value={value}
               onChange={(e) => {
                 setValue(e.target.value);
-                // setError(null);
+                setError(null);
               }}
               type="text"
               placeholder="Shorten a link here..."
               className={`px-[40px] w-full py-[12px] rounded-lg outline-none ${
                 error
-                  ? "border-2 border-red-500 md:mb-0 mb-4 placeholder:text-red-300"
+                  ? "border-2 border-red-500 md:mb-0 mb-6 placeholder:text-red-300"
                   : ""
               }`}
             />
@@ -83,12 +90,35 @@ const Shorten: React.FC = () => {
             </button>
           </div>
           {error && (
-            <p className="text-red-500 text-sm mt-[-4.3rem] md:mt-[-1rem] italic">
+            <p className="text-red-500 text-sm mt-[-5.6rem] md:mt-[-1rem] italic">
               {error}
             </p>
           )}
         </div>
       </section>
+      {success && shortenedLink && (
+        <section className="md:flex justify-center">
+          <main className="w-full py-4 px-2 rounded-lg md:flex justify-between bg-[#FFFFFF]">
+            {/* Display shortened link */}
+            <div className="border-b-[1px] md:border-none pb-2 truncate border-[#E7E6E9]">
+              {shortenedLink.length > 8
+                ? `${shortenedLink.substring(0, 30)}...`
+                : shortenedLink}
+            </div>
+            <div className="md:flex items-center gap-4 md:mt-0 mt-2">
+              <div className="text-[#7AE1E1]">{shortenedLink}</div>
+              <button
+                onClick={handleCopy}
+                className={`${
+                  copied ? "bg-[#3A3054] text-white" : "bg-[#7AE1E1] text-white"
+                } w-full md:w-auto px-4 py-2`}
+              >
+                {copied ? "Copied" : "Copy"}
+              </button>
+            </div>
+          </main>
+        </section>
+      )}
     </main>
   );
 };
